7a934f60c542f99c877112e32d35cbaa5bf31531
Mutala Mutala <jonamut12.live@gmail.com>
2024-08-03 19:09:43 -0400

    Add extension methode

---
+++ Extensions/NorthwindServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using northwind_project.Models;
+using northwind_project.Services;
+using northwind_project.Services.Interface;
+
+namespace northwind_project.Extensions
+{
+    public static class NorthwindServiceRegistration
+    {
+        public static IServiceCollection AddNorthwindServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,6 +1,18 @@
-﻿namespace northwind_project.Services
+﻿using northwind_project.Models;
+using northwind_project.Services.Interface;
+
+namespace northwind_project.Services
 {
-    public class ProductService 
+    public class ProductService : IProductService
     {
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
