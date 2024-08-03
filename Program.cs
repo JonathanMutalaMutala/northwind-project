454523e00fadacfb63aa7920f2cb415759e556ad
Mutala Mutala <jonamut12.live@gmail.com>
2024-08-03 19:21:12 -0400

    Add data base to the context service

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using northwind_project.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<NorthwindContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("northwindConnectionString"));
+});
+
 
 var app = builder.Build();
 
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,10 +1,18 @@
-﻿using northwind_project.Models;
+﻿using northwind_project.Data;
+using northwind_project.Models;
 using northwind_project.Services.Interface;
 
 namespace northwind_project.Services
 {
     public class ProductService : IProductService
     {
+        private readonly NorthwindContext _northwindContext;
+
+        public ProductService(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+
         public Product GetProductById(int id)
         {
             throw new NotImplementedException();
