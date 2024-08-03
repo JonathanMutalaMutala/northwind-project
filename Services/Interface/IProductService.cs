9f84da63af56cef4c7c6bfd7ad1c49e3f4afe867
Mutala Mutala <jonamut12.live@gmail.com>
2024-08-03 18:46:27 -0400

    Add product service

---
+++ Services/Interface/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using northwind_project.Models;
+
+namespace northwind_project.Services.Interface
+{
+    public interface IProductService
+    {
+        public List<Product> GetProducts();
+
+        public Product GetProductById(int id);
+
+    }
+}
