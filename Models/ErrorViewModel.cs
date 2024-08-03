5c8a8f1340f3c568cf335a8f0d40d8ac2a5807e8
Mutala Mutala <jonamut12.live@gmail.com>
2024-08-03 03:45:42 -0400

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace northwind_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
